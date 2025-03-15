574d2bb8c68839a6744547ed0d26c80c85deb80d
Nick Stins <n.stins@hotmail.com>
2025-03-15 17:22:14 +0100

    chrome app icon

--- next.config.js
+++ next.config.js
@@ -1,14 +1,40 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-/** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
+  publicExcludes: ['!icon-*.png'],
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(?:gstatic|googleapis)\.com\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'google-fonts',
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 365 * 24 * 60 * 60
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60
+        }
+      }
+    }
+  ]
 });
 
 module.exports = withPWA({
   // Your existing Next.js configuration can go here
   reactStrictMode: true,
+  images: {
+    domains: []
+  }
 });
